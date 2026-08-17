2de4ac092f499945d104bb66d73ea97287b5bec6
Heloisa <heloisa.f.santos@aluno.senai.br>
2026-08-17 17:38:50 -0300

    alterações diversas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,67 +4,26 @@ import { HomeComponent } from './component/home-component/home-component';
 import { AtletaComponent } from './component/atleta/atleta-component/atleta-component';
 import { AtletaListComponent } from './component/atleta/atleta-list-component/atleta-list-component';
 
-import { CorridaComponent } from './pages/corrida/corrida.component';
-
-import { CorridasDisponiveisComponent } from './pages/corridas-disponiveis/corridas-disponiveis.component';
-
-import { InscricaoComponent } from './pages/inscricao/inscricao.component';
-
-
 export const routes: Routes = [
-
-  {
-    path: '',
-    redirectTo: 'home',
-    pathMatch: 'full'
-  },
-
-
-  {
-    path: 'home',
-    component: HomeComponent
-  },
-
-
-  {
-    path: 'cadastroatleta',
-    component: AtletaComponent
-  },
-
-
-  {
-    path: 'cadastroatleta/:id',
-    component: AtletaComponent
-  },
-
-
-  {
-    path: 'listaAtleta',
-    component: AtletaListComponent
-  },
-
-
-  {
-    path: 'corrida',
-    component: CorridaComponent
-  },
-
-
-  {
-    path: 'cadastrocorrida',
-    component: CorridaComponent
-  },
-
-
-  {
-    path: 'corridas-disponiveis',
-    component: CorridasDisponiveisComponent
-  },
-
-
-  {
-    path: 'inscricao',
-    component: InscricaoComponent
-  }
-
+    {
+        path: "",
+        redirectTo: "/home",
+        pathMatch: "full"
+    },
+    {
+        path: "home",
+        component: HomeComponent
+    },
+    {
+        path: "cadastroAtleta",
+        component: AtletaComponent,
+    },
+    {
+        path: "cadastroAtleta/id",
+        component: AtletaComponent,
+    },
+    {
+        path: "listaAtleta",
+        component: AtletaListComponent,
+    }
 ];
--- src/app/component/atleta/atleta-list-component/atleta-list-component.ts
+++ src/app/component/atleta/atleta-list-component/atleta-list-component.ts
@@ -1,33 +1,10 @@
-import { Component, OnInit } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { RouterModule } from '@angular/router';
-import { AtletaService } from '../../../services/atleta-service';
-import { Pessoa } from '../../../models/Pessoa';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-atleta-list-component',
   standalone: true,
-  imports: [CommonModule, RouterModule],
+  imports: [],
   templateUrl: './atleta-list-component.html',
-  styleUrl: './atleta-list-component.css'
+  styleUrl: './atleta-list-component.css',
 })
-export class AtletaListComponent implements OnInit {
-  atletas: Pessoa[] = [];
-
-  constructor(private atletaService: AtletaService) {}
-
-  ngOnInit(): void {
-    this.carregarAtletas();
-  }
-
-  carregarAtletas(): void {
-    this.atletaService.listarAtletas().subscribe({
-      next: (dados) => {
-        this.atletas = dados;
-      },
-      error: (erro) => {
-        console.error('Erro ao carregar atletas', erro);
-      }
-    });
-  }
-}
+export class AtletaListComponent {}
