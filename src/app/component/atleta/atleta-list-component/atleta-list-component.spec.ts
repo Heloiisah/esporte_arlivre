71d71bfb7d65c34529b0c214922af35f4e983d57
Heloisa <heloisa.f.santos@aluno.senai.br>
2026-08-17 18:00:44 -0300

    alterações

--- src/app/component/atleta/atleta-list-component/atleta-list-component.spec.ts
+++ src/app/component/atleta/atleta-list-component/atleta-list-component.spec.ts
@@ -1,17 +1,17 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { HomeComponent } from './home-component';
+import { AtletaListComponent } from './atleta-list-component';
 
-describe('HomeComponent', () => {
-  let component: HomeComponent;
-  let fixture: ComponentFixture<HomeComponent>;
+describe('AtletaListComponent', () => {
+  let component: AtletaListComponent;
+  let fixture: ComponentFixture<AtletaListComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [HomeComponent],
+      imports: [AtletaListComponent],
     }).compileComponents();
 
-    fixture = TestBed.createComponent(HomeComponent);
+    fixture = TestBed.createComponent(AtletaListComponent);
     component = fixture.componentInstance;
     await fixture.whenStable();
   });
