4a29ab91819368fc0b6bb9c07b2916e29635a613
heloisa <heloisa.f.santos10@aluno.senai.br>
2026-08-26 16:08:11 -0300

    alterando o corrida lista

--- src/app/component/corrida/corrida-lista-component/corrida-lista-component.spec.ts
+++ src/app/component/corrida/corrida-lista-component/corrida-lista-component.spec.ts
@@ -1,36 +1,92 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { provideHttpClient } from '@angular/common/http';
+import { of } from 'rxjs';
 
 import { CorridaListaComponent } from './corrida-lista-component';
+import { CorridaService } from '../../../service/corrida/corrida-service';
+import { Corrida } from '../../../models/Corrida';
+
 
 describe('CorridaListaComponent', () => {
 
   let component: CorridaListaComponent;
   let fixture: ComponentFixture<CorridaListaComponent>;
 
+  let service: CorridaService;
+
+
   beforeEach(async () => {
 
-    await TestBed.configureTestingModule({
+    TestBed.configureTestingModule({
 
-      imports: [CorridaListaComponent],
+      imports: [
+        CorridaListaComponent
+      ],
 
       providers: [
+        CorridaService,
         provideHttpClient()
       ]
 
-    }).compileComponents();
+    });
 
-    fixture = TestBed.createComponent(CorridaListaComponent);
+    fixture = TestBed.createComponent(
+      CorridaListaComponent
+    );
 
     component = fixture.componentInstance;
 
-    await fixture.whenStable();
+    service = TestBed.inject(
+      CorridaService
+    );
 
   });
 
-  it('should create', () => {
+
+  it('deve criar o componente', () => {
 
     expect(component).toBeTruthy();
+
+  });
+
+
+  it('Deve retornar as corridas', () => {
+
+    const corridas: Corrida[] = [
+
+      {
+        id: 1,
+        descricao_corrida: 'Corrida de Aracaju'
+      } as Corrida,
+
+      {
+        id: 2,
+        descricao_corrida: 'Corrida de Sergipe'
+      } as Corrida
+
+    ];
+
+
+    spyOn(service, 'listarCorridas')
+      .and.returnValue(of(corridas)); 
+
+
+    component.listar();
+
+
+    expect(
+      component.listaCorridas().length
+    ).toBe(2);
+
+
+    expect(
+      component.listaCorridas()[0].descricao_corrida
+    ).toBe('Corrida de Aracaju');
+
+
+    expect(
+      component.listaCorridas()[1].descricao_corrida
+    ).toBe('Corrida de Sergipe');
 
   });
 
